f56c32cd87718016009a8e940c63001426fb8549
Szabolcs Toth <tsz@purzelbaum.hu>
2015-10-12 22:26:37 +0300

    Added file input and conditions

--- dp.rb
+++ dp.rb
@@ -9,55 +9,60 @@
 
 # Global variables
 ornArray = Array.new
+tempArray = Array.new
 lineHash = Hash.new
 prevORN = ""
-prevTYPE = ""
+# prevTYPE = ""
 prevFEAT = ""
 qty = 0
 
- while (line = gets)
-    # leaves the headlines
-    if not line =~ /SYSN|\*|----|ORDER|\d{4}-\d{2}-\d{2}\s\d{2}.\d{2}.\d{2}/
-        # read every line into string
-        str = line
-        str.length
-        # TODO need to put as regexp above
-       if line =~ /[a-zA-Z]/
-       		if prevORN != str[10, 6]
-        		puts "\n"
-        		puts str[10, 6]
-        		puts "------"
+if ARGV.length > 1
+    file= ARGV.first
 
-        		if ornArray.length == 0
-        			qty += 1
-        			lineHash = {"ORN" => str[10,6], "TYPE" => str[18,4], "MOD" => str[24,3], "DESC" => str[48,30]}
-        			ornArray.push(linesHash)
-        		end
-        		
-        	end
+    f = File.open(file, "r")
+    f.each_line { |line|
+        # leaves the headlines
+        if not line =~ /SYSN|\*|----|ORDER|\d{4}-\d{2}-\d{2}\s\d{2}.\d{2}.\d{2}/
+            # read every line into string
+            str = line
+            str.length
+            # TODO need to put as regexp above
+           if line =~ /[a-zA-Z]/
+           		if prevORN != str[10, 6]
+            	   if tempArray.length == 0
+                        qty += 1
+                        lineHash = {"ORN" => str[10,6], "TYPE" => str[18,4], "MOD" => str[24,3], "QTY" => qty, "DESC" => str[48,30]}
+                        tempArray.push(lineHash)
+                        prevORN = str[10,6]
+            	   end
+                else
+                    if tempArray.length != 0
+                        if prevFEAT != str[28,6]
+                            ornArray.push(tempArray.last)
+                            tempArray = []
+                            qty = 1
+                            lineHash = {"ORN" => str[10,6], "TYPE" => str[18,4], "MOD" => str[24,3], "QTY" => qty, "DESC" => str[48,30]}
+                            tempArray.push(lineHash)
+                        else
+                            qty += 1
+                            lineHash = {"ORN" => str[10,6], "TYPE" => str[18,4], "MOD" => str[24,3], "QTY" => qty, "DESC" => str[48,30]}
+                            tempArray.push(lineHash)
+                        end
+                    end
+            	end
+            end
         end
-    end
+    }
+    f.close
+else
+    puts "You need files to proceed."
 end
 
 =begin
-if prevTYPE != str[18,4]
-        			if qty != 0
-	        			print "#{str[18,4]}\t#{str[24,3]}\t#{qty}\n"
-	        			prevTYPE = str[18,4]
-	        		end
-        		else
-        			print "#{str[18,4]}\t#{str[24,3]}\n"
-        			prevTYPE = str[18,4]
-        			qty = qty + 1
-        		end
-
-        		prevORN = str[10,6]
-        	else
-        		if prevTYPE != str[18,4]
-        			print "#{str[18,4]}\t#{str[24,3]}\n"
-        			prevTYPE = str[18,4]
-        		else
-        			print "#{str[18,4]}\t#{str[24,3]}\n"
-        			prevTYPE = str[18,4]
-        		end
+z = ARGV.length
+puts z
+puts "first: #{ARGV.first}"
+for i in 1..ARGV.length-1
+    puts "#{i+1}. : #{ARGV[i]}"
+end  
 =end
